88ad5247164b0e587a994a6a495635ea91ac8097
pradeep-techlanz <pradeep.kuamr@techlanz.com>
2025-10-25 01:57:43 +0530

    added

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Wind Turbine Optimization API", version="3.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # Allows all origins. Replace with a list of allowed domains if needed
+    allow_origins=["http://127.0.0.1:5500","http://127.0.0.1:3000","http://127.0.0.1:3030"],  # Allows all origins. Replace with a list of allowed domains if needed
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods
     allow_headers=["*"],  # Allows all headers
